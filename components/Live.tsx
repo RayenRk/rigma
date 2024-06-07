5625d280a9d7940af663a76265fedf4933999076
RayenRk <rakkadrayen@gmail.com>
2024-06-07 15:45:35 +0100

    🟩 Live cursor message

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,16 @@
 import LiveCursors from "./cursor/LiveCursors";
 import { useMyPresence, useOthers } from "@/liveblocks.config";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
 
 const Live = () => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const [cursorState, setCursorState] = useState({
+    mode : CursorMode.Hidden,
+  })
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
@@ -16,7 +22,8 @@ const Live = () => {
     updateMyPresence({ cursor: { x, y } });
   }, []);
 
-  const handlePointerLeave = useCallback(() => {
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    setCursorState({ mode: CursorMode.Hidden });
     updateMyPresence({ cursor: null, message: null });
   }, []);
 
@@ -32,8 +39,17 @@ const Live = () => {
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
-      className="h-[100vh] w-full flex justify-center items-center text-center border-5 border-green-500"
+      className="h-[100vh] w-full flex justify-center items-center text-center"
     >
+
+      {cursor && (
+        <CursorChat
+          cursor = {cursor}
+          cursorState = {cursorState}
+          setCursorState = {setCursorState}
+          updateMyPresence = {updateMyPresence}
+        />
+        )}
       <LiveCursors others={others} />
       <h1 className="text-2xl text-white">Rigma</h1>
 
--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,7 +1,35 @@
-const CursorChat = () => {
-  return (
-    <div>CursorChat</div>
-  )
-}
+import CursorSVG from "@/public/assets/CursorSVG";
+import { CursorChatProps, CursorMode } from "@/types/type";
+import { transform } from "next/dist/build/swc";
 
-export default CursorChat
+// {cursorState.mode === CursorMode.Chat && (
+// )}
+
+const CursorChat = ({
+  cursor,
+  cursorState,
+  setCursorState,
+  updateMyPresence,
+}: CursorChatProps) => {
+  return (
+    <div
+      className="absolute top-0 left-0"
+      style={{
+        transform: `translateX(
+  ${cursor.x}px) translateY(${cursor.y}px)`,
+      }}
+    >
+        <>
+          <CursorSVG color="#000" />
+          <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+            {cursorState.previousMessage && (
+                <div>{cursorState.previousMessage}</div>
+            )}
+            <input />
+          </div>
+        </>
+    </div>
+  );
+};
+
+export default CursorChat;
