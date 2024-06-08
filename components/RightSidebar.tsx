ebf4d0602da35a6a36594ef661d2d0f839d0e122
RayenRk <rakkadrayen@gmail.com>
2024-06-08 16:24:41 +0100

    🟦 Colors and export feature

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useRef } from "react";
 import Export from "./settings/Export";
 import Dimensions from "./settings/Dimensions";
 import Text from "./settings/Text";
@@ -14,6 +14,9 @@ function RightSidebar({
   isEditingRef,
   syncShapeInStorage,
 }: RightSidebarProps) {
+
+  const colorInputRef = useRef(null);
+  const strokeInputRef = useRef(null);
 
   const handleInputChange = (property: string, value: string) => {
     if (!isEditingRef.current) isEditingRef.current = true;
@@ -55,8 +58,20 @@ function RightSidebar({
       fontWeight={elementAttributes.fontWeight}
       handleInputChange={handleInputChange}
       />
-      <Color />
-      <Color />
+      <Color 
+      inputRef={colorInputRef}
+      attribute={elementAttributes.fill}
+      attributeType="fill"
+      placeholder="color"
+      handleInputChange={handleInputChange}
+      />
+      <Color 
+      inputRef={strokeInputRef}
+      attribute={elementAttributes.stroke}
+      attributeType="stroke"
+      placeholder="stroke"
+      handleInputChange={handleInputChange}
+      />
       <Export />
     </section>
   );
